b944f56a92c2bec99c246f80ba6a6ef383dfe045
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-21 00:56:51 +0300

    Refactor tests

--- src/test/scala/goldrush/MinerSpec.scala
+++ src/test/scala/goldrush/MinerSpec.scala
@@ -1,13 +1,11 @@
 package goldrush
 
 import java.util.concurrent.atomic.AtomicLong
-
-import cats.Id
 import goldrush.Miner.Explorator
 import monix.eval.Coeval
 import monix.execution.Scheduler
 import monix.execution.schedulers.SchedulerService
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{Assertion, BeforeAndAfterAll}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -16,60 +14,28 @@ class MinerSpec extends AnyFlatSpec with BeforeAndAfterAll with Matchers {
   override def afterAll() = {
     s.shutdown()
   }
-  val areaSide = 100
-  val area = Area(0, 0, areaSide, areaSide)
+  private val areaSide = 100
+  private val area = Area(0, 0, areaSide, areaSide)
   private val locations: Seq[(Int, Int)] = area.locations
 
   "Explorator" should "explore all the treasures" in {
-    val explore: Area => Coeval[ExploreResponse] =
-      a => Coeval.pure(ExploreResponse(a, a.locations.size))
-
-    val foundPositions = Miner
-      .explorator(explore)(area, locations.size)
-      .toListL
-      .runSyncUnsafe()
-
-    foundPositions.sorted shouldBe locations.map { case (x, y) =>
-      (x, y, 1)
-    }.sorted
+    testExplorator(area)((_, _) => true)
   }
 
   it should "explore only the places with treasures" in {
-    def containsTreasure(x: Int, y: Int): Int = (x + y) % 2
-    def treasures(a: Area): Int = a.locations.map { case (x, y) =>
-      containsTreasure(x, y)
-    }.sum
-
-    val explore: Area => Coeval[ExploreResponse] =
-      a => Coeval.delay(ExploreResponse(a, treasures(a)))
-
-    val foundPositions = Miner
-      .explorator(explore)(area, treasures(area))
-      .toListL
-      .runSyncUnsafe()
-
-    val expected = locations
-      .map { case (x, y) =>
-        (x, y, containsTreasure(x, y))
-      }
-      .filter(_._3 != 0)
-    foundPositions.sorted shouldBe expected.sorted
+    testExplorator(area)((x, y) => (x + y) % 2 == 0)
   }
 
   it should "explore only the places with treasures2" in {
     println(s"size of field: $areaSide")
 
-    val batches: Seq[(String, (Area => Coeval[ExploreResponse]) => (Area, Int) => Explorator)] =
-      Seq(1, 2, 4, 5, 8, 13, 30, 100, 1000).flatMap(maxStep =>
-        Seq(
-          s"exploratorBatched$maxStep" -> Miner.exploratorBatched[Coeval](maxStep)
-        )
-      )
-    val methods: Seq[
-      (String, (Area => Coeval[ExploreResponse]) => (Area, Int) => Explorator)
-    ] = batches ++ Seq(
-      "exploratorBinary" -> Miner.exploratorBinary[Coeval]
-    )
+    val batches: Seq[(String, ExploreMethod)] =
+      for {
+        maxStep <- Seq(1, 2, 4, 5, 8, 13, 30, 100, 1000)
+      } yield s"exploratorBatched$maxStep" -> Miner.exploratorBatched[Coeval](maxStep)
+
+    val methods: Seq[(String, ExploreMethod)] =
+      batches ++ Seq("exploratorBinary" -> Miner.exploratorBinary[Coeval])
 
     methods.map { case (methodName, exploreMethod) =>
       Seq(25).map { frequency =>
@@ -115,6 +81,28 @@ class MinerSpec extends AnyFlatSpec with BeforeAndAfterAll with Matchers {
         foundPositions.sorted shouldBe expected.sorted
       }
     }
+  }
+
+  type ExploreMethod = (Area => Coeval[ExploreResponse]) => (Area, Int) => Explorator
+
+  private def makeExplorator(allArea: Area)(containsTreasure: (Int, Int) => Boolean): Explorator = {
+    def treasures(area: Area): Int = area.locations.map { case (x, y) =>
+      if (containsTreasure(x, y)) 1 else 0
+    }.sum
+
+    Miner.explorator { area =>
+      Coeval.delay(ExploreResponse(area, treasures(area)))
+    }(allArea, treasures(allArea))
+  }
+
+  private def testExplorator(allArea: Area)(containsTreasure: (Int, Int) => Boolean): Assertion = {
+    val explorator = makeExplorator(allArea)(containsTreasure)
+    val actualResult = explorator.toListL.runSyncUnsafe()
+
+    val expectations = allArea.locations
+      .filter { case (x, y) => containsTreasure(x, y) }
+      .map { case (x, y) => (x, y, 1) }
+    actualResult.sorted shouldBe expectations.sorted
   }
 
 }
