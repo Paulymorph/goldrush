e74dc9e885c7dc30de0eec71ec18f308de0837c0
aira <ai535@ya.ru>
2021-03-23 00:58:52 +0300

    default__2021-03-23_00-58-52

--- build_and_push.sh
+++ build_and_push.sh
@@ -12,16 +12,16 @@ fi
 DOCKER_TAG="${INPUT_PARAM}__$(date +%F_%H-%M-%S)"
 DOCKER_TAG=`echo "$DOCKER_TAG" | tr '[:upper:]' '[:lower:]'`
 echo "DOCKER_TAG=$DOCKER_TAG"
+
 echo "package goldrush\nobject DockerTag { val dockerTag: String = \"$DOCKER_TAG\" }" >./src/main/scala/goldrush/DockerTag.scala
 
 cp -r ./src ./src-backups/$DOCKER_TAG
 
 git add .
 git commit -m "$DOCKER_TAG"
-git status
-#git push -u origin "$DOCKER_TAG"
-#git reset HEAD~1 --soft
+git push origin HEAD:$DOCKER_TAG
+git reset HEAD~1 --soft
 
-#docker build -t "$DOCKER_TAG" ./
-#docker tag "$DOCKER_TAG" stor.highloadcup.ru/rally/utopian_falcon
-#docker push stor.highloadcup.ru/rally/utopian_falcon
+docker build -t "$DOCKER_TAG" ./
+docker tag "$DOCKER_TAG" stor.highloadcup.ru/rally/utopian_falcon
+docker push stor.highloadcup.ru/rally/utopian_falcon
--- src/main/scala/goldrush/DockerTag.scala
+++ src/main/scala/goldrush/DockerTag.scala
@@ -1,2 +1,2 @@
 package goldrush
-object DockerTag { val dockerTag: String = "default__2021-03-23_00-57-30" }
+object DockerTag { val dockerTag: String = "default__2021-03-23_00-58-52" }
