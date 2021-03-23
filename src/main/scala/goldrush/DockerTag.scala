e0ee0b797e6661cae57d01158972144df367cdb5
aira <ai535@ya.ru>
2021-03-23 11:41:13 +0300

    2021-03-23_11-41-13__winner__default

---
+++ src/main/scala/goldrush/DockerTag.scala
@@ -0,0 +1,2 @@
+package goldrush
+object DockerTag { val dockerTag: String = "2021-03-23_11-41-13__winner__default" }
--- src/main/scala/goldrush/Main.scala
+++ src/main/scala/goldrush/Main.scala
@@ -5,7 +5,9 @@ import monix.eval.{Task, TaskApp}
 import sttp.client3.asynchttpclient.monix.AsyncHttpClientMonixBackend
 
 object Main extends TaskApp {
-  override def run(args: List[String]): Task[ExitCode] =
+  override def run(args: List[String]): Task[ExitCode] = {
+    println(s"Started. ${DockerTag.dockerTag}")
+    Constants.print()
     for {
       baseUrl <- Config.getBaseUrl[Task]
       backend <- AsyncHttpClientMonixBackend()
@@ -13,4 +15,5 @@ object Main extends TaskApp {
       miner = Miner[Task](client)
       _ <- miner.mine
     } yield ExitCode.Success
+  }
 }
