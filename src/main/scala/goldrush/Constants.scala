cd4a1743c222182e3b08fbc5617dfaf95fa65343
aira <ai535@ya.ru>
2021-03-23 23:02:28 +0300

    2021-03-23_23-02-28__winner__default

--- src/main/scala/goldrush/Constants.scala
+++ src/main/scala/goldrush/Constants.scala
@@ -3,12 +3,16 @@ package goldrush
 object Constants {
 
   val digParallelism = 32
-  val exploreParallelism = digParallelism * 2
+  val exploreParallelism = digParallelism * 4
   val licenceParallelism = digParallelism * 2
-  val cashParallelism = 6
+  val cashParallelism = 8
   val maxExploreArea = 10
   val clientCapDelay = 5
   val clientFullJitter = 1
+
+  val licenceBufferSize = 10
+  val exploreBufferSize = 256
+  val digBufferSize = 256
 
   def print(): Unit = {
     println(
--- src/main/scala/goldrush/DockerTag.scala
+++ src/main/scala/goldrush/DockerTag.scala
@@ -1,2 +1,2 @@
 package goldrush
-object DockerTag { val dockerTag: String = "2021-03-23_11-41-13__winner__default" }
+object DockerTag { val dockerTag: String = "2021-03-23_23-02-28__winner__default" }
