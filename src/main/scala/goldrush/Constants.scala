94ecb1de80b066801a133b49d1e176b5eaf42369
aira <ai535@ya.ru>
2021-03-23 21:57:47 +0300

    2021-03-23_21-57-47__winner__default

--- src/main/scala/goldrush/Constants.scala
+++ src/main/scala/goldrush/Constants.scala
@@ -2,13 +2,17 @@ package goldrush
 
 object Constants {
 
-  val digParallelism = 32
-  val exploreParallelism = digParallelism * 2
-  val licenceParallelism = digParallelism * 2
+  val digParallelism = 24
+  val exploreParallelism = digParallelism * 4
+  val licenceParallelism = digParallelism
   val cashParallelism = 6
   val maxExploreArea = 10
   val clientCapDelay = 5
   val clientFullJitter = 1
+
+  val licenceBufferSize = 10
+  val exploreBufferSize = 256
+  val cashBufferSize = 128
 
   def print(): Unit = {
     println(
