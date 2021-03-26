fed18577eea556dfbbc4b89d3675c12d0e6ba3b0
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 20:51:42 +0300

    Revert "Try unbound storage with coins recycling"
    This reverts commit e7ef075e

--- src/main/scala/goldrush/GoldStore.scala
+++ src/main/scala/goldrush/GoldStore.scala
@@ -1,8 +1,7 @@
 package goldrush
 
-import cats.Monad
+import cats.Applicative
 import cats.effect.{Concurrent, ContextShift}
-import cats.syntax.flatMap._
 import cats.syntax.functor._
 import cats.syntax.traverse._
 import goldrush.GoldStore.Coin
@@ -17,21 +16,22 @@ object GoldStore {
   type Coin = Int
 }
 
-class GoldStoreImpl[F[_]: Monad] private (store: ConcurrentQueue[F, Coin]) extends GoldStore[F] {
+class GoldStoreImpl[F[_]: Applicative] private (store: ConcurrentQueue[F, Coin])
+    extends GoldStore[F] {
   override def put(coins: Coin*): F[Unit] = {
     coins.traverse(store.tryOffer).void
   }
 
   override def tryTake(coinsNumber: Coin): F[Seq[Coin]] = {
-    store.drain(0, coinsNumber).flatTap { coins =>
-      store.offerMany(coins)
+    Seq.fill(coinsNumber)(coinsNumber).flatTraverse { _ =>
+      store.tryPoll.map(_.toSeq)
     }
   }
 }
 
 object GoldStoreImpl {
   def apply[F[_]: Concurrent: ContextShift](storageSize: Int): F[GoldStore[F]] = {
-    ConcurrentQueue.unbounded[F, Coin]().map { store =>
+    ConcurrentQueue.bounded[F, Coin](storageSize).map { store =>
       new GoldStoreImpl[F](store)
     }
   }
