506198dd53f95e1a39464af713e04c221c8368b0
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 10:37:51 +0300

    Try delayed statistics logging

--- src/main/scala/goldrush/Main.scala
+++ src/main/scala/goldrush/Main.scala
@@ -1,28 +1,31 @@
 package goldrush
 
-import cats.effect.ExitCode
+import cats.effect.{ExitCode, Timer}
 import monix.eval.{Task, TaskApp}
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import sttp.client3.asynchttpclient.monix.AsyncHttpClientMonixBackend
 import build.BuildInfo
+import scala.concurrent.duration._
 
 object Main extends TaskApp {
   override def run(args: List[String]): Task[ExitCode] =
-    Slf4jLogger.create[Task].flatMap { logger =>
-      logger.info(BuildInfo.version) *>
-        Statistics[Task].bracket { statistics =>
-          for {
-            baseUrl <- Config.getBaseUrl[Task]
-            backend <- AsyncHttpClientMonixBackend()
-            client <- ClientImpl[Task](baseUrl, backend).map(StatisticsClient.wrap(statistics))
-            _ <- Miner[Task](client).use(_.mine)
-          } yield ExitCode.Success
-        } { statistics =>
-          for {
-            info <- statistics.getInfo
-            _ <- logger.info(info.toString)
-          } yield ()
+    for {
+      logger <- Slf4jLogger.create[Task]
+      _ <- logger.info(BuildInfo.version)
+      statistics <- Statistics[Task]
+      _ <- Task.deferAction { implicit scheduler =>
+        Task.delay {
+          scheduler.scheduleOnce(9.minutes + 50.seconds) {
+            statistics.getInfo.flatMap { info =>
+              logger.info(info.toString)
+            }.runAsyncAndForget
+          }
         }
-    }
+      }
+      baseUrl <- Config.getBaseUrl[Task]
+      backend <- AsyncHttpClientMonixBackend()
+      client <- ClientImpl[Task](baseUrl, backend).map(StatisticsClient.wrap(statistics))
+      _ <- Miner[Task](client).use(_.mine)
+    } yield ExitCode.Success
 }
