4d659ea970d3ab9589e7d921f82efbaa8e0fff34
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 20:49:25 +0300

    Fix issue license bug

--- src/main/scala/goldrush/StatisticsClient.scala
+++ src/main/scala/goldrush/StatisticsClient.scala
@@ -11,7 +11,7 @@ class StatisticsClient[F[_]: FlatMap] private (underlying: Client[F], statistics
   override def listLicenses(): F[Seq[License]] = underlying.listLicenses()
 
   override def issueLicense(coins: Int*): F[License] =
-    underlying.issueLicense().flatTap(l => statistics.issuedLicense(coins, l))
+    underlying.issueLicense(coins: _*).flatTap(l => statistics.issuedLicense(coins, l))
 
   override def explore(area: Area): F[ExploreResponse] = underlying.explore(area)
 
