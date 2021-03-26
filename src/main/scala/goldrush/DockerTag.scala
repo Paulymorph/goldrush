074c3371988462304dc0b1ce329520d38071d840
aira <ai535@ya.ru>
2021-03-26 15:33:42 +0300

    2021-03-26_15-33-42__winner__default

--- src/main/scala/goldrush/DockerTag.scala
+++ src/main/scala/goldrush/DockerTag.scala
@@ -1,2 +1,2 @@
 package goldrush
-object DockerTag { val dockerTag: String = "2021-03-26_00-50-55__winner__default" }
+object DockerTag { val dockerTag: String = "2021-03-26_15-33-42__winner__default" }
