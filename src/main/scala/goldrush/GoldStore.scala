e7ef075ebabbb52808c46fd3331d80386b649aea
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 20:04:50 +0300

    Try unbound storage with coins recycling

--- src/main/scala/goldrush/GoldStore.scala
+++ src/main/scala/goldrush/GoldStore.scala
@@ -1,7 +1,8 @@
 package goldrush
 
-import cats.Applicative
+import cats.Monad
 import cats.effect.{Concurrent, ContextShift}
+import cats.syntax.flatMap._
 import cats.syntax.functor._
 import cats.syntax.traverse._
 import goldrush.GoldStore.Coin
@@ -16,22 +17,21 @@ object GoldStore {
   type Coin = Int
 }
 
-class GoldStoreImpl[F[_]: Applicative] private (store: ConcurrentQueue[F, Coin])
-    extends GoldStore[F] {
+class GoldStoreImpl[F[_]: Monad] private (store: ConcurrentQueue[F, Coin]) extends GoldStore[F] {
   override def put(coins: Coin*): F[Unit] = {
     coins.traverse(store.tryOffer).void
   }
 
   override def tryTake(coinsNumber: Coin): F[Seq[Coin]] = {
-    Seq.fill(coinsNumber)(coinsNumber).flatTraverse { _ =>
-      store.tryPoll.map(_.toSeq)
+    store.drain(0, coinsNumber).flatTap { coins =>
+      store.offerMany(coins)
     }
   }
 }
 
 object GoldStoreImpl {
   def apply[F[_]: Concurrent: ContextShift](storageSize: Int): F[GoldStore[F]] = {
-    ConcurrentQueue.bounded[F, Coin](storageSize).map { store =>
+    ConcurrentQueue.unbounded[F, Coin]().map { store =>
       new GoldStoreImpl[F](store)
     }
   }
