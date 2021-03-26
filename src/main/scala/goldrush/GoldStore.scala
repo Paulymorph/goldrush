0b69f20644946826c55515f6ae60298121f1ebca
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 20:22:24 +0300

    Put all coins into licenses

--- src/main/scala/goldrush/GoldStore.scala
+++ src/main/scala/goldrush/GoldStore.scala
@@ -1,6 +1,8 @@
 package goldrush
 
 import cats.Monad
+import cats.data.Chain
+import cats.effect.concurrent.Ref
 import cats.effect.{Concurrent, ContextShift}
 import cats.syntax.flatMap._
 import cats.syntax.functor._
@@ -17,21 +19,20 @@ object GoldStore {
   type Coin = Int
 }
 
-class GoldStoreImpl[F[_]: Monad] private (store: ConcurrentQueue[F, Coin]) extends GoldStore[F] {
+class GoldStoreImpl[F[_]: Monad] private (store: Ref[F, Chain[Coin]]) extends GoldStore[F] {
   override def put(coins: Coin*): F[Unit] = {
-    coins.traverse(store.tryOffer).void
+    val chainedCoins = Chain.fromSeq(coins)
+    store.update(_ ++ chainedCoins)
   }
 
   override def tryTake(coinsNumber: Coin): F[Seq[Coin]] = {
-    store.drain(0, coinsNumber).flatTap { coins =>
-      store.offerMany(coins)
-    }
+    store.get.map(_.toList)
   }
 }
 
 object GoldStoreImpl {
   def apply[F[_]: Concurrent: ContextShift](storageSize: Int): F[GoldStore[F]] = {
-    ConcurrentQueue.unbounded[F, Coin]().map { store =>
+    Ref.of(Chain.empty[Coin]).map { store =>
       new GoldStoreImpl[F](store)
     }
   }
