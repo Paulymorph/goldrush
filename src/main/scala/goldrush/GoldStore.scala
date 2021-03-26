6c329f09f550932005f45b4727a77cab7b32cf75
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2021-03-26 20:51:38 +0300

    Revert "Put all coins into licenses"
    This reverts commit 0b69f206

--- src/main/scala/goldrush/GoldStore.scala
+++ src/main/scala/goldrush/GoldStore.scala
@@ -1,8 +1,6 @@
 package goldrush
 
 import cats.Monad
-import cats.data.Chain
-import cats.effect.concurrent.Ref
 import cats.effect.{Concurrent, ContextShift}
 import cats.syntax.flatMap._
 import cats.syntax.functor._
@@ -19,20 +17,21 @@ object GoldStore {
   type Coin = Int
 }
 
-class GoldStoreImpl[F[_]: Monad] private (store: Ref[F, Chain[Coin]]) extends GoldStore[F] {
+class GoldStoreImpl[F[_]: Monad] private (store: ConcurrentQueue[F, Coin]) extends GoldStore[F] {
   override def put(coins: Coin*): F[Unit] = {
-    val chainedCoins = Chain.fromSeq(coins)
-    store.update(_ ++ chainedCoins)
+    coins.traverse(store.tryOffer).void
   }
 
   override def tryTake(coinsNumber: Coin): F[Seq[Coin]] = {
-    store.get.map(_.toList)
+    store.drain(0, coinsNumber).flatTap { coins =>
+      store.offerMany(coins)
+    }
   }
 }
 
 object GoldStoreImpl {
   def apply[F[_]: Concurrent: ContextShift](storageSize: Int): F[GoldStore[F]] = {
-    Ref.of(Chain.empty[Coin]).map { store =>
+    ConcurrentQueue.unbounded[F, Coin]().map { store =>
       new GoldStoreImpl[F](store)
     }
   }
